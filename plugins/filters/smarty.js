d5a7b1919d180cb296c67fa68f92181c6f6a1e26
Matthew Lyon <matthew@lyonheart.us>
2014-03-06 16:14:26 -0800

    fix smarty plugin to not nobr headlines

--- plugins/filters/smarty.js
+++ plugins/filters/smarty.js
@@ -1,5 +1,9 @@
 var typogr = require('typogr');
 
 module.exports = function(text) {
-  return typogr.typogrify(text);
+  t = typogr(text).chain().amp()
+  if (text.split(' ').length > 2) {
+    t = t.widont()
+  }
+  return t.smartypants().initQuotes().caps().ord().value()
 }
