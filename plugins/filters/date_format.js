2143dbfaee0f99be87081c30a6712e1c16557dda
Matthew Lyon <matthew@lyonheart.us>
2014-11-19 22:39:05 -0800

    remove old wintersmith plugins

--- plugins/filters/date_format.js
+++
@@ -1,6 +0,0 @@
-var moment = require('moment');
-
-module.exports = function(date, format) {
-  return moment(date).format(format);
-}
-
--- plugins/filters/smarty.js
+++
@@ -1,9 +0,0 @@
-var typogr = require('typogr');
-
-module.exports = function(text) {
-  t = typogr(text).chain().amp()
-  if (text.split(' ').length > 2) {
-    t = t.widont()
-  }
-  return t.smartypants().initQuotes().caps().ord().value()
-}
